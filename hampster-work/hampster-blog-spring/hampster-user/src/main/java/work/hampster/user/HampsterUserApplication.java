dc6afaebdf68d340eaa3677eae0b5048d33f4e5a
AbhamsterBall <ajh415@hotmail.com>
2024-06-20 13:09:09 +0800

    update website structure

---
+++ hampster-work/hampster-blog-spring/hampster-user/src/main/java/work/hampster/user/HampsterUserApplication.java
@@ -0,0 +1,13 @@
+package work.hampster.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HampsterUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Config.class, args);
+	}
+
+}
