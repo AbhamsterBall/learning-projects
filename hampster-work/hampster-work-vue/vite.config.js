80e4422e08f3bada5776f4faa6a248ed05cbc896
AbhamsterBall <ajh415@hotmail.com>
2024-09-23 15:13:44 +0800

    sync

--- hampster-work/hampster-work-vue/vite.config.js
+++ hampster-work/hampster-work-vue/vite.config.js
@@ -8,52 +8,72 @@ export default defineConfig({
   plugins: [vue()],
   transpileDependencies: true,
   server: {
-    host: '0.0.0.0',
-    port: 81,
-    open: true,
-    proxy: {
+      host: '0.0.0.0',
+      port: 8989,
+      open: true,
+      proxy: {
+          // '^/blog-api^': { // '/api'是代理标识，用于告诉node，url前面是/api的就是使用代理的
+          //     target: "http://localhost:80/astaff.php/", //目标地址，一般是指后台服务器地址
+          //     changeOrigin: true, //是否跨域
+          // },
         '^/blog-api': {
           target: 'http://localhost:9090/', // 8081本地后端地址
           changeOrigin: true, //允许跨域
           // ws: false,
           rewrite: (path) => path.replace(/^\/blog-api/, ''),
         },
-        '/user': {
-          target: 'http://localhost:8082/', // 本地后端地址
-          changeOrigin: true, //允许跨域
-          ws: false
-        },
-        '/web': {
-          target: 'http://localhost:80/', // 本地后端地址
-          changeOrigin: true, //允许跨域
-          ws: false
-        }
-    }
+      }
   },
-  devServer: {
-    // proxy: "http://localhost:8081",
-    // proxy: {
-    //   '/blog-api': {
-    //     target: 'http://localhost:80',
-    //     router: () => 'http://localhost:8081', // 8081本地后端地址
-    //     changeOrigin: true, //允许跨域
-    //     ws: false,
-    //     pathRewrite: {
-    //       '^/blog-api': '', // 去掉路径中的/blog-api
-    //     },
-    //   },
-    //   '/user': {
-    //     target: 'http://localhost:8082/', // 本地后端地址
-    //     changeOrigin: true, //允许跨域
-    //     ws: false
-    //   },
-    //   '/web': {
-    //     target: 'http://localhost:80/', // 本地后端地址
-    //     changeOrigin: true, //允许跨域
-    //     ws: false
-    //   }
-    // },
-  }
+    // proxy: "http://localhost:9090",
+  //   proxy: {
+  //       // '^': {
+  //       //     target: 'http://localhost:9090/json/search/blur/%E6%98%AF/1', // 8081本地后端地址
+  //       //     changeOrigin: true, //允许跨域
+  //       //     // ws: false,
+  //       //     // rewrite: (path) => path.replace(/^\/blog-api/, ''),
+  //       // },
+  //       '^/blog-api': {
+  //         target: 'http://localhost:9090/', // 8081本地后端地址
+  //         changeOrigin: true, //允许跨域
+  //         // ws: false,
+  //         // rewrite: (path) => path.replace(/^\/blog-api/, ''),
+  //       },
+  //       '/user': {
+  //         target: 'http://localhost:8082/', // 本地后端地址
+  //         changeOrigin: true, //允许跨域
+  //         ws: false
+  //       },
+  //       '/web': {
+  //         target: 'http://localhost:80/', // 本地后端地址
+  //         changeOrigin: true, //允许跨域
+  //         ws: false
+  //       }
+  //   }
+  // },
+  // devServer: {
+  // //   proxy: "http://localhost:9090",
+  //   proxy: {
+  //     '^/blog-api': {
+  //       target: 'http://localhost:9090',
+  //       // router: () => 'http://localhost:8081', // 8081本地后端地址
+  //       changeOrigin: true, //允许跨域
+  //       ws: false,
+  //       pathRewrite: {
+  //         '^/blog-api': '', // 去掉路径中的/blog-api
+  //       },
+  // //   //   },
+  // //   //   '/user': {
+  // //   //     target: 'http://localhost:8082/', // 本地后端地址
+  // //   //     changeOrigin: true, //允许跨域
+  // //   //     ws: false
+  // //   //   },
+  // //   //   '/web': {
+  // //   //     target: 'http://localhost:80/', // 本地后端地址
+  // //   //     changeOrigin: true, //允许跨域
+  // //   //     ws: false
+  //     }
+  //   }
+  // }
 })
 // let { CopyWebpackPlugin } = require("copy-webpack-plugin")
 // module.exports = defineConfig({
