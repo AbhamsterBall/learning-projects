0290d20996b9e19de0d01cd2e93df707b396ce98
AbhamsterBall <ajh415@hotmail.com>
2024-09-25 13:18:59 +0800

    sync

--- hampster-work/hampster-blog-spring/hampster-user/src/main/java/work/hampster/user/UserApplication.java
+++ hampster-work/hampster-blog-spring/hampster-user/src/main/java/work/hampster/user/UserApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HampsterUserApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Config.class, args);
