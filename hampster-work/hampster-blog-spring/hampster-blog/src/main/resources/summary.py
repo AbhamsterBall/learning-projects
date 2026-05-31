8f4915377b042d6a7c7aded491f3ecdef7e28186
AbhamsterBall <ajh415@hotmail.com>
2026-05-31 11:15:08 +0800

    even under local python environment could run summary.py

--- hampster-work/hampster-blog-spring/hampster-blog/src/main/resources/summary.py
+++ hampster-work/hampster-blog-spring/hampster-blog/src/main/resources/summary.py
@@ -36,7 +36,7 @@ import sys
 import json
 
 sys.stdout.reconfigure(encoding='utf-8')
-local_directory = "F:\\JAVA\\20231010\\hampster-work\\hampster-blog-spring\\hampster-blog\\src\\main\\resources\\local_model"
+local_directory = "D:\\HAMPSTER-FOLDER\\hampster-work\\hampster-blog-spring\\hampster-blog\\src\\main\\resources\\local_model"
 
 # 加载本地模型和分词器
 model = AutoModelForQuestionAnswering.from_pretrained(local_directory)
@@ -47,9 +47,17 @@ nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
 
 # 获取预测
 QA_input = {
-    'question': '主要内容是什么?',
-    'context': sys.argv[1]
+    'question': '主要内容是什么?', # 主要内容是什么?
 }
+if len(sys.argv) > 1:
+    QA_input['context'] = sys.argv[1]
+else:
+    QA_input['context'] = """北京时间2023年5月30日，中国载人航天工程办公室宣布，神舟十六号载人飞船在酒泉卫星发射中心成功发射。
+        此次任务由航天员景海鹏、朱杨柱和桂海潮组成乘组，其中景海鹏担任指令长。
+        这是中国空间站应用与发展阶段的首个载人飞行任务，也是神舟系列飞船的第十六次飞行任务。
+        神舟十六号与空间站组合体成功对接后，三名航天员将进驻天和核心舱，开展空间科学实验和技术试验。
+        值得一提的是，航天员桂海潮是北京航空航天大学的教授，也是中国首位非军人出身的航天员。"""
+
 res = nlp(QA_input)
 format_res = json.dumps(res, ensure_ascii=False)
 
